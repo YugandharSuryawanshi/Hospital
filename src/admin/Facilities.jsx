d2371d20df7330655b783ea7c164d749a3d48ab8
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2025-12-23 18:44:28 +0530

    Add code in facility

--- src/admin/Facilities.jsx
+++ src/admin/Facilities.jsx
@@ -3,8 +3,24 @@ import React from "react";
 
 export default function Facilities() {
     return (
-        <div>
-            <h1>Facilities</h1>
-        </div>
+        <>
+            <div className="container">
+                <h1 className="text-center text-danger">* Facilities *</h1>
+                <button className="btn btn-danger float-right">Add Facility</button>
+            </div><br /><br />
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Cashless Facilities</h5>
+                                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
