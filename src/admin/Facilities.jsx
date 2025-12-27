fb03a25784dd2e0fc24e7ba67ece2c2b13a61ebf
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2025-12-27 18:29:54 +0530

    Add code in facility

--- src/admin/Facilities.jsx
+++ src/admin/Facilities.jsx
@@ -5,6 +5,55 @@ import axios from "axios";
 export default function Facilities() {
 
     const [facilities, setFacilities] = useState([]);
+    const [facility_id, setId] = useState("");
+    const [facility_name, setName] = useState("");
+    const [facility_desc, setFacility_desc] = useState("");
+    const [facility_image, setFacility_image] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append("facility_id", facility_id);
+        formData.append("facility_name", facility_name);
+        formData.append("facility_desc", facility_desc);
+        formData.append("facility_image", facility_image);
+
+        const token = localStorage.getItem("adminToken");
+
+
+        const payload = {
+            facility_id: facility_id,
+            facility_name: facility_name,
+            facility_desc: facility_desc,
+        };
+        console.log(facility_name);
+        console.log(facility_desc);
+        console.log(facility_image);
+
+
+
+        try {
+            const res = await axios.post("http://localhost:5000/api/admin/addFacility", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            if (res.status !== 201) {
+                throw new Error("Failed to create appointment.");
+            }
+
+            alert("Facility added successfully!");
+            setImage(null);
+            e.target.reset();
+        } catch (err) {
+            console.error(err);
+            alert("Error adding facility");
+        }
+
+    }
 
     useEffect(() => {
         axios
@@ -25,21 +74,32 @@ export default function Facilities() {
         <>
             <div className="container">
                 <h1 className="text-center text-danger">* Facilities *</h1>
+                <button className="btn badge-warning float-right ml-2">Show Facilities</button>
                 <button className="btn btn-danger float-right">Add Facility</button>
             </div><br /><br />
 
             <div className="container">
                 <div className="row">
                     <div className="col-md-2"></div>
-                    <div className="col-md-8">
-                        <form action="" className="text-center">
-                            <div className="form-group">
-                                <input type="text" className="form-control" id="facility" placeholder="Enter Facility" />
+                    <div className="col-md-8 text-center align-content-center">
+                        <form onSubmit={handleSubmit}>
+                            <div className="col-md-12">
+                                <div className="row">
+                                    <div className="form-group col-md-4">
+                                        <label className="form-label">Facility Name</label>
+                                        <input type="text" className="form-control" value={facility_name} onChange={(e) => setName(e.target.value)} required placeholder="Enter Facility" />
+                                    </div>
+                                    <div className="form-group col-md-4">
+                                        <label className="form-label">Description</label>
+                                        <input type="text" className="form-control" value={facility_desc} onChange={(e) => setFacility_desc(e.target.value)} id="description" placeholder="Enter Description" />
+                                    </div>
+                                    <div className="form-group col-md-4">
+                                        <label className="form-label">Facility Image</label>
+                                        <input type="file" className="form-control" id="image" placeholder="Upload Image" onChange={(e) => setFacility_image(e.target.files[0])} />
+                                    </div>
+                                </div>
+                                <button type="submit" className="btn btn-danger mb-4">Add</button>
                             </div>
-                            <div className="form-group">
-                                <input type="text" className="form-control" id="description" placeholder="Enter Description" />
-                            </div>
-                            <button type="submit" className="btn btn-danger mb-4">Add</button>
                         </form>
                     </div>
                 </div>
