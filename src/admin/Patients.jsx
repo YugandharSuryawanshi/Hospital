4a2bf509f689e6e337f0ab034c6d4e409226c3a6
Yugandhar Suryawanshi <yugandharsuryawanshi0@gmail.com>
2026-01-21 00:11:13 +0530

    Patient work are complete From admin Side

--- src/admin/Patients.jsx
+++ src/admin/Patients.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { NavLink , useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Patient() {
     const [patients, setPatients] = useState([]);
@@ -11,10 +11,10 @@ export default function Patient() {
 
     //Pagination
     const [currentPage, setCurrentPage] = useState(1);
-    const usersPerPage = 10;
+    const usersPerPage = 6;
 
     // Add Patient
-    const navigate = useNavigate();
+    const [updateID, setUpdateID] = useState("");
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
@@ -25,6 +25,62 @@ export default function Patient() {
     const [gender, setGender] = useState("");
     const [loading, setLoading] = useState(false);
 
+    // If after edit came into add then reset all fields
+    const changeState = () => {
+        setName("");
+        setEmail("");
+        setPhone("");
+        setAddress("");
+        setAge("");
+        setGender("");
+        setPassword("");
+        setConfirmPassword("");
+        setViewMode("add");
+    };
+
+    // After New Patient Add came in list page with update list
+    const changeStateList = () => {
+        fetchPatients();
+        setViewMode("list");
+    };
+
+    // Handle Update is update Patients
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+
+        const user_id = updateID;
+        const payload = {
+            user_name: name,
+            user_email: email,
+            user_phone: phone,
+            user_address: address,
+            user_age: age,
+            user_gender: gender,
+        };
+
+        try {
+            const res = await axios.put(
+                `http://localhost:4000/api/admin/updateUser/${user_id}`,
+                payload,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            if (res.status === 200) {
+                alert("Patient Updated Successfully");
+                changeStateList();
+            }
+        } catch (err) {
+            console.error(err);
+            alert("Update failed");
+        }
+    };
+
+    // Add New Patient or Entry
     const handleRegister = async (e) => {
         e.preventDefault();
 
@@ -60,7 +116,7 @@ export default function Patient() {
 
             if (res.status === 201) {
                 alert("Patient Added Successfully.");
-                viewMode = "list";
+                changeStateList();
             }
 
         } catch (err) {
@@ -71,7 +127,28 @@ export default function Patient() {
         }
     };
 
-    useEffect(() => {
+    // Fetch Single Patient Data and put this in fields.
+    const handleEditClick = (patient) => {
+        setUpdateID(patient);
+        axios.get(`http://localhost:4000/api/admin/getUser/${patient}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
+            },
+        }).then((res) => {
+            console.log(res.data);
+            setName(res.data.user_name);
+            setEmail(res.data.user_email);
+            setPhone(res.data.user_phone);
+            setAddress(res.data.user_address);
+            setAge(res.data.user_age);
+            setGender(res.data.user_gender);
+            setViewMode("edit");
+        })
+
+    };
+
+    // Fetch/ Get All Patients
+    const fetchPatients = async () => {
         axios.get("http://localhost:4000/api/admin/getAllUsers", {
             headers: {
                 Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
@@ -79,13 +156,33 @@ export default function Patient() {
         })
             .then((res) => {
                 setPatients(res.data);
+                setViewMode("list");
             })
             .catch((err) => {
                 console.log(err);
             });
+    }
+
+    // Delete Patient
+    const handleDelete = (id) => {
+        axios
+            .delete(`http://localhost:4000/api/admin/deleteUser/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
+                },
+            })
+            .then((res) => {
+                alert(res.data.message);
+                fetchPatients();
+            })
+            .catch((err) => console.error(err));
+    };
+
+    useEffect(() => {
+        fetchPatients();
     }, []);
 
-    /* SEARCH LOGIC */
+    /* Search Logic */
     const filteredPatients = patients.filter((patient) => {
         const keyword = searchTerm.toLowerCase();
 
@@ -94,11 +191,13 @@ export default function Patient() {
             patient.user_name?.toLowerCase().includes(keyword) ||
             patient.user_email?.toLowerCase().includes(keyword) ||
             patient.user_phone?.toLowerCase().includes(keyword) ||
-            patient.user_age?.toString().includes(keyword)
+            patient.user_age?.toString().includes(keyword) ||
+            patient.user_gender?.toLowerCase().includes(keyword) ||
+            patient.user_address?.toLowerCase().includes(keyword)
         );
     });
 
-    /* PAGINATION LOGIC */
+    /* Pagination Logic */
     const indexOfLastUser = currentPage * usersPerPage;
     const indexOfFirstUser = indexOfLastUser - usersPerPage;
     const currentPatients = filteredPatients.slice(
@@ -111,27 +210,22 @@ export default function Patient() {
     return (
         <>
             <h1 className="font-weight-bold text-danger text-center">Patients</h1>
-            <h5 className="text-muted font-weight-bold text-center">
-                Manage Your Patients
-            </h5>
+            <h5 className="text-muted font-weight-bold text-center"> Manage Your Patients </h5>
 
-            {/* HEADER CONTROLS  */}
+            {/* header Part  */}
             <div className="container-fluid">
                 <div className="card border-0">
                     <div className="row align-items-center">
                         <div className="col-md-8"></div>
 
                         <div className="col-md-3 bg-warning p-2 d-flex align-items-center">
-                            <button
-                                className="btn btn-danger mr-2"
-                                onClick={() => setViewMode("add")}
-                            >
+                            <button className="btn btn-danger mr-2" onClick={() => changeState()} >
                                 Add
                             </button>
 
                             <button
                                 className="btn btn-danger me-2"
-                                onClick={() => setViewMode("list")}
+                                onClick={() => changeStateList()}
                             >
                                 List
                             </button>
@@ -153,7 +247,7 @@ export default function Patient() {
                 </div>
             </div>
 
-            {/* PATIENT LIST */}
+            {/* Patient List */}
             {viewMode === "list" && (
                 <div className="container mt-4">
                     <div className="card shadow-sm border-0">
@@ -166,6 +260,7 @@ export default function Patient() {
                             </span>
                         </div>
 
+                        {/* Card Prints are here */}
                         <div className="card-body p-0">
                             <div className="table-responsive">
                                 <table className="table table-hover table-bordered align-middle mb-0">
@@ -209,10 +304,10 @@ export default function Patient() {
                                                         </span>
                                                     </td>
                                                     <td>
-                                                        <button className="btn btn-sm btn-outline-primary m-2">
+                                                        <button className="btn btn-sm btn-outline-primary m-2" onClick={() => handleEditClick(patient.user_id)}>
                                                             <i className="fa fa-edit"></i>
                                                         </button>
-                                                        <button className="btn btn-sm btn-outline-danger">
+                                                        <button className="btn btn-sm btn-outline-danger" onClick={() => { handleDelete(patient.user_id) }}>
                                                             <i className="fa fa-trash"></i>
                                                         </button>
                                                     </td>
@@ -230,23 +325,16 @@ export default function Patient() {
                             </div>
                         </div>
 
-                        {/* PAGINATION */}
+                        {/* pagination is here */}
                         {totalPages > 1 && (
                             <div className="card-footer bg-white">
                                 <nav>
                                     <ul className="pagination justify-content-center mb-0">
                                         {[...Array(totalPages)].map((_, index) => (
-                                            <li
-                                                key={index}
-                                                className={`page-item ${currentPage === index + 1 ? "active" : ""
-                                                    }`}
-                                            >
-                                                <button
-                                                    className="page-link"
-                                                    onClick={() => setCurrentPage(index + 1)}
-                                                >
-                                                    {index + 1}
-                                                </button>
+                                            <li key={index}
+                                                className={`page-item ${currentPage === index + 1 ? "active" : ""}`}>
+                                                <button className="page-link btn btn-outline-danger"
+                                                    onClick={() => setCurrentPage(index + 1)}>{index + 1}</button>
                                             </li>
                                         ))}
                                     </ul>
@@ -335,13 +423,70 @@ export default function Patient() {
             )}
 
 
-
-
-            {/* PATIENT Edit */}
+            {/* Patient Edit */}
             {viewMode === "edit" && (
-                <div className="container">
-                    <h1>Edit Patient</h1>
-                    <h1>Welcome to Edit Patient Page Are ano</h1>
+                <div className="container d-flex justify-content-center align-items-center mt-4">
+                    <div className="card shadow-lg border-0 rounded-4 p-4 w-100">
+                        <form onSubmit={handleUpdate}>
+                            <div className="row">
+
+                                <div className="col-md-6 mb-3">
+                                    <label className="form-label fw-semibold">Full Name</label>
+                                    <input type="text" className="form-control form-control-lg" value={name}
+                                        onChange={(e) => setName(e.target.value)} placeholder="Enter your name" />
+                                </div>
+
+                                <div className="col-md-6 mb-3">
+                                    <label className="form-label fw-semibold">Email</label>
+                                    <input type="email" className="form-control form-control-lg" value={email}
+                                        onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
+                                </div>
+
+                                <div className="col-md-6 mb-3">
+                                    <label className="form-label fw-semibold">Phone</label>
+                                    <input type="text" className="form-control form-control-lg" value={phone}
+                                        onChange={(e) => setPhone(e.target.value)} placeholder="Enter phone number" />
+                                </div>
+
+                                <div className="col-md-6 mb-3">
+                                    <label className="form-label fw-semibold">Address</label>
+                                    <input type="text" className="form-control form-control-lg" value={address}
+                                        onChange={(e) => setAddress(e.target.value)} placeholder="Enter address" />
+                                </div>
+
+                                <div className="col-md-6 mb-3">
+                                    <label className="form-label fw-semibold">Age</label>
+                                    <input type="text" className="form-control form-control-lg" value={age}
+                                        onChange={(e) => setAge(e.target.value)} placeholder="Enter Age" />
+                                </div>
+
+                                <div className="col-md-6 mb-3">
+                                    <label className="form-label fw-semibold">Gender</label><br />
+                                    <select className="form-select w-75 h-50 form-select-lg" value={gender}
+                                        onChange={(e) => setGender(e.target.value)} >
+                                        <option value="">Select Gender</option>
+                                        <option value="Male">Male</option>
+                                        <option value="Female">Female</option>
+                                        <option value="Other">Other</option>
+                                    </select>
+                                </div>
+
+                            </div>
+
+                            <div className="d-grid mt-3 text-center">
+                                <button type="submit" className="btn btn-danger btn-lg shadow-sm" disabled={loading} >
+                                    {loading ? "Updating..." : "Update"}
+                                </button>
+                            </div>
+
+                            <p className="text-center mt-3 mb-0">
+                                Already have an account?{" "}
+                                <NavLink to="/login" className="fw-semibold">
+                                    Login here
+                                </NavLink>
+                            </p>
+                        </form>
+                    </div>
                 </div>
             )}
 
